a23841670138bfcb57ecc41f1945ae1d0a3fd90b
Shazid86 <adjustmentattitude5@gmail.com>
2024-10-23 12:35:58 +0600

    final

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+// btn-1
+document.getElementById('btn-1').addEventListener('click', function(){
+  window.open("https://www.rokomari.com/", "_blank");
+})
+
+// btn-2
+document.getElementById('btn-2').addEventListener('click', function(){
+  window.open("https://oneummahbd.shop/", "_blank");
+})
+
+// btn-3
+document.getElementById('btn-3').addEventListener('click', function(){
+  window.open("https://oneummahbd.shop/", "_blank");
+})
+
+// btn-4
+document.getElementById('btn-4').addEventListener('click', function(){
+  window.open("https://oneummahbd.shop/", "_blank");
+})
+
+// btn-5
+document.getElementById('btn-5').addEventListener('click', function(){
+  window.open("https://believerssign.com.bd/", "_blank");
+})
+
+// btn-6
+document.getElementById('btn-6').addEventListener('click', function(){
+  window.open("https://believerssign.com.bd/", "_blank");
+})
+
+// btn-7
+document.getElementById('btn-7').addEventListener('click', function(){
+  window.open("https://believerssign.com.bd/", "_blank");
+})
